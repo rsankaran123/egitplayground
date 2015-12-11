419a9ba9217687e6e00b859945dd2c9f09acd21c
Ranjani Sankaran <ranjani.sankaran@lochbridge.com>
2015-12-11 11:58:07 -0500

    commit changes

--- first/src/main/java/com/vogella/git/first/GitTest.java
+++ first/src/main/java/com/vogella/git/first/GitTest.java
@@ -2,7 +2,7 @@ package com.vogella.git.first;
 
 public class GitTest {
   public static void main(String[] args) {
-    System.out.println("Git is Cool and Confusing, I am trying to figure it out..");
+    System.out.println("Git is Cool and Confusing, I am trying to figure it out.");
   }
 } 
 
